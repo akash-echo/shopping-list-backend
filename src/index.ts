e417518a11633aa991f99dc9ae5f96bb4579a833
akash-echo <akash@echologyx.com>
2025-06-08 04:26:25 +0600

    Keep DB url into the env file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import app from "./app";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const PORT = 8000;
-const MONGO_URI = "mongodb://127.0.0.1:27017/shoppinglist";
+dotenv.config(); // Load environment variables
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+if (!process.env.MONGO_URI) {
+  throw new Error("Missing MONGO_URI environment variable");
+}
+
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
